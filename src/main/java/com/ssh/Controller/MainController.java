fff83a5f4fd50772471e908889659125aeea8b62
a329336057 <329336057@qq.com>
2019-01-31 13:33:22 +0800

    提交

--- src/main/java/com/ssh/Controller/MainController.java
+++ src/main/java/com/ssh/Controller/MainController.java
@@ -1,15 +1,29 @@
 package com.ssh.Controller;
 
+import com.ssh.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
+    @Autowired(required=true)
+    private PersonService personService;
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test(){
         System.out.print("sadadada");
 //        实际返回的是views/test.jsp ,spring-mvc.xml中配置过前后缀
         return "test";
+    }
+
+
+    @RequestMapping(value = "/savePerson", method = RequestMethod.GET)
+    @ResponseBody
+    public String savePerson() {
+        personService.savePerson();
+        return "success!";
     }
 }
---
+++ src/main/java/com/ssh/repository/DomainRepository.java
@@ -0,0 +1,26 @@
+package com.ssh.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 这个接口定义一些十分通用的方法
+ * Created by XRom
+ * On 11/16/2017.11:52 PM
+ */
+public interface DomainRepository<T, s> {
+    T load(s id);
+    T get(s id);
+
+    List<T> findAll();
+
+    void persist(T entity);
+
+    s save(T entity);
+
+    void saveOrUpdate(T entity);
+
+    void delete(s id);
+
+    void flush();
+}
